397051e0fee2445d4c7b7995800c03ab445e0f13
kingsonrisa <awsingjose@gmail.com>
2023-06-21 16:20:18 -0400

    aws-deploy-tf-67

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@
  resource "aws_vpc" "vpc-passion" {
    cidr_block       =  var.cidr-vpc-passion[count.index]
    instance_tenancy = "default"
-   main_route_table_id = false
    count = 3
    tags = {
      Name = "VPC-${count.index}"
