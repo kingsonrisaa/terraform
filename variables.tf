3e60b41b63cfce3471efcd9ffc787b0e1f7815d1
kingsonrisa <awsingjose@gmail.com>
2023-06-21 14:53:53 -0400

    AWS_v30

--- variables.tf
+++ variables.tf
@@ -4,10 +4,6 @@ variable "aws_region" {
 }
 
 variable "cidr-vpc-passion" {
-  type = string
-  description = "vpc passion"
-}
-variable "cidr-vpc-passion" {
   description = "CIDR block VPC"
   type = list(string)
 }
