1c00c9139abb6bf0422f03548190017fb921d4a2
kingsonrisa <awsingjose@gmail.com>
2023-06-21 15:14:38 -0400

    aws_vpc_create

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
  resource "aws_vpc" "vpc-passion" {
    cidr_block       =  var.cidr-vpc-passion[count.index]
    instance_tenancy = "default"
+   main_route_table_id = false
    count = 3
    tags = {
      Name = "VPC-${count.index}"
