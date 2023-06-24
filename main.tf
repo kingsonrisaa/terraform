5cd364ce2a3fc57aa49aa41c2cf8071ef4953242
Jose Miguel <106883490+kingsonrisaa@users.noreply.github.com>
2023-06-24 03:41:29 -0400

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-#Create 3 VPC-AWS-us-east-1
- resource "aws_vpc" "vpc-passion" {
-   cidr_block       =  var.cidr-vpc-passion[count.index]
-   instance_tenancy = "default"
-   count = 3
-   tags = {
-     Name = "VPC-${count.index}"
-   }
- }
