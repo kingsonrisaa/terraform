0d30cfb84f70dfb3d799eb70db8c536add030a02
kingsonrisa <awsingjose@gmail.com>
2023-07-06 01:34:35 -0400

    v2

--- main.tf
+++ main.tf
@@ -24,11 +24,13 @@ resource "aws_s3_bucket_policy" "bucket-policy-terraform" {
     "Statement": [
     {
       "Effect": "Allow",
+      "Principal": "*",
       "Action": "s3:ListBucket",
       "Resource": "arn:aws:s3:::${aws_s3_bucket.terraformstate.id}/*"
     },
     {
       "Effect": "Allow",
+      "Principal": "*",
       "Action": ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
       "Resource": "arn:aws:s3:::${aws_s3_bucket.terraformstate.id}/*"
     }
