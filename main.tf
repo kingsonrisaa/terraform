414768e942747b2ef624dd9fc22bb2805af3c114
kingsonrisa <awsingjose@gmail.com>
2023-07-06 13:44:59 -0400

    v1

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-#create resource
+#create resource s3 and bucket policy
 resource "aws_s3_bucket" "terraformstate" {
   bucket = "mytfstatetestbucket020202"
   tags = {
@@ -46,3 +46,16 @@ resource "aws_s3_bucket_policy" "bucket-policy-terraform" {
 }
 EOF
 }
+
+#create dynamodb table
+
+resource "aws_dynamodb_table" "statelock-dynamodb" {
+  name = "statelock-dynamodb"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "tflockid"
+  attribute {
+    name = "tflockid"
+    type = "S"
+  }
+}
+
