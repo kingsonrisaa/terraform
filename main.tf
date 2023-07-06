a1f0c0b4fc74713464c3e7f988ee0a11470c4a92
kingsonrisa <awsingjose@gmail.com>
2023-07-06 13:55:26 -0400

    v1

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket_policy" "bucket-policy-terraform" {
     {
       "Effect": "Allow",
       "Principal": {
-                "AWS": "arn user iam"
+                "AWS": "arn:aws:iam::743512395119:user/User_Terraform"
             },
       "Action": "s3:ListBucket",
       "Resource": [ "arn:aws:s3:::${aws_s3_bucket.terraformstate.id}",
